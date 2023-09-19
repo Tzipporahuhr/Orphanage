5c68a5e1bc28eb86803035ed75d9e6886f09000e
Tzipporahuhr <113932909+Tzipporahuhr@users.noreply.github.com>
2023-09-19 12:39:56 -0400

    revised orphanage

--- Orphanage.sql
+++ Orphanage.sql
@@ -1,13 +1,13 @@
-/* The Brooklyn orphanage has been up and running since 1967. There are many students who come and go each year. The orphanage 
+/*   The Brooklyn orphanage has been up and running since 1967. There are many students who come and go each year. The orphanage 
 is starting to revamp and modernize a little. They had started with renovating the building. They now want to organize their data and files of 
 all their  students since the orphange began. They will create an organized database which will include a list of the following:
 StudentsFirstname, StudentsLastName,Floor, RoomNumber, DateOfBirth, City, DateEnrolled, Dateleft, Gender.
 
 Reports needed:
-1) Show a list of  all students who enrolled to the orphanage in the 2o century and their age at the time when they entered the sem
-2) Show a list of Students who are not from the tri-state area, and their age when they entered the orphanage
-3) Show me the Avg how how long students tend to stay in the orphanage and an average of boys and girls
-4) Write me a format Id for any student whos last name begins with a-p, looking like this ex: (Smith, Peter-07/06/2001)
+1) Show a list of  all students who enrolled in  the orphanage in the 2o century and their age at the time when they entered the sem
+2) Show a list of Students who are not from the tri-state area(the tri state area will include brooklyn and new jersey), and their age when they entered the orphanage
+3) Show me the Avg how  long students tend to stay in the orphanage and an average of boys and girls
+4) Write me a format Id for the students, it should look like this ex: (Smith, Peter-07/06/2001).
 
 Question:
 How old does one have to be to come to the orphanage?
@@ -30,20 +30,20 @@ Note: There are some a bunch of sets of siblings
       The DateLeft cannot be before Date enrolled
       Noone gets marked down for Datenrolled prior to the date they come
 Sample Data:
-Ron, Smith, 3, 1, 07/07/2000,'Alabama','December 14, 07/07/2010, null'
-Osher, Sharon, 3, 2, 03/03/2002, 'Alaska','January 3, 2012, null
-Peter, Smith, 3, 3, 05/03/1999,	'Arizona','February 14, 2009', march 7, 2021
-Sally, Cohen, 3, 4, 04/03/ 2000, Lakewood, June 15, march 4, 2000
-Shuli Cohen, 3,5, 02/03/1970	'California','September 9, 1850', null
-chavi Cohen 2, 6,  04/03/1970, Flatbush, August 1, 1981', september 2, 1990
-Tzvi, Gold 2, 7, 09/08/2000	'Connecticut','January 9, 2011, null
-Yaakov Gole 2, 8, 06/04/2005	connecticut,'December 7, 2011', null
-Shalom, Feinkork, 2, 9, 01/02/2002,	'Florida',March 3, 2013', march 3, 2015
-Sara, Bell, 2,10, 03/02/2000	Monsey,'January 2, 2011', May 4, 2012
-Shira, Well 1, 11, 05/04/2001	'Hawaii','August 21, 2011', null
-chaim, well, 1,12, 04/03/1980	hawaii,July 3, 1991, march 4, 2001'
-Hadassa, CarPool, 1, 13 02/02/2002	'Illinois','December 3, 2013', null
-Chavi, Hello, 1, 14, 02/03/2003	'Indiana',December 11, 2014, null
-Shaindy, See 1, 15, 02/06/2005	'Iowa','December 28, 2011'
+Ron, Smith, male, 3, 1, 07/07/2000,'Alabama','December 14, 07/07/2010, null'
+Osher, Sharon, male, 3, 2, 03/03/2002, 'Alaska','January 3, 2012, null
+Peter, Smith, male,  3, 3, 05/03/1999,	'Arizona','February 14, 2009', march 7, 2021
+Sally, Cohen, female, 3, 4, 04/03/ 2000, new jersey, June 15, march 4, 2000
+Shuli Cohen, female,  3,5, 02/03/1970	'California','September 9, 1850', null
+chavi Cohen, female, 2, 6,  04/03/1970, brooklyn, August 1, 1981', september 2, 1990
+Tzvi, Gold, male, 2, 7, 09/08/2000	'Connecticut','January 9, 2011, null
+Yaakov Gole, male 2, 8, 06/04/2005	connecticut,'December 7, 2011', null
+Shalom, Feinkork, male,  2, 9, 01/02/2002,	'Florida',March 3, 2013', march 3, 2015
+Sara, Bell, female, 2,10, 03/02/2000	new york,'January 2, 2011', May 4, 2012
+Shira, Well, female, 1, 11, 05/04/2001	'Hawaii','August 21, 2011', null
+chaim, well, male, 1,12, 04/03/1980	hawaii,July 3, 1991, march 4, 2001'
+Hadassa, CarPool, female, 1, 13 02/02/2002	'Illinois','December 3, 2013', null
+Chavi, Hello, female,  1, 14, 02/03/2003	'Indiana',December 11, 2014, null
+Shaindy, See, female,  1, 15, 02/06/2005	'Iowa','December 28, 2011'
  
 
